10115792d52cd0e661ca59f3040812de3b6680a0
Fearless-Badger <micah.najacht@gmail.com>
2024-12-16 22:34:30 -0500

    Script Progress

--- download_data.ps1
+++ download_data.ps1
@@ -1,10 +1,12 @@
 $dataset = "shrutimechlearn/churn-modelling"
 $zip_file = "churn-modelling.zip"
-$output_dir = $pwd
+$output_dir = "$env:OUTPUT_PATH"
+
+Write-Output "dir : $output_dir"
 
 try {
     Write-Output "Attempting Download Dataset : $dataset"
-    kaggle datasets download -d $dataset -p $outputdir --unzip --force
+    kaggle datasets download -d $dataset -p "$outputdir" --unzip --force
 
     if (Test-Path "$outputdir\$zip_file"){
         Write-Output "Success!"
